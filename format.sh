a767e7c88863b53b64a844082c2f673365803056
Igor Demura <igor.demura@gmail.com>
2021-02-27 23:13:17 -0800

    test

--- format.sh
+++ format.sh
@@ -4,3 +4,5 @@ find . -iname '*.h' -exec clang-format -i {} \;
 find . -iname '*.c' -exec clang-format -i {} \;
 find . -iname '*.hpp' -exec clang-format -i {} \;
 find . -iname '*.cpp' -exec clang-format -i {} \;
+
+# test
